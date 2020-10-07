abeafc019e283afe582c79c55ef7810c4418f331
man nguyen <nguyenlyminhman@gmail.com>
2020-10-07 11:20:31 +0700

    Create Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Nav from './Nav';
+import NoteList from './NoteList';
+import NoteForm from './NoteForm';
+
+
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    
+  }
+  render() {
+    return (
+      <div>
+      <Nav />
+      <div className="container">
+        <div className="row">
+          <NoteList />
+          <NoteForm />
+        </div>
+      </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
